d10be0a310fccf37f814e2ec50efc927e60f0cbd
RiddhiDeogade <deogaderiddhi@gmail.com>
2025-06-05 00:48:14 +0530

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,14 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCrnOrtzp-XgUDo46z8qAJYEccaCFTj4Ho",
-  authDomain: "ppartner-db.firebaseapp.com",
-  projectId: "ppartner-db",
-  storageBucket: "ppartner-db.firebasestorage.app",
-  messagingSenderId: "867428021513",
-  appId: "1:867428021513:web:98b5d72ff901edbfeccfd8",
-  measurementId: "G-KZMF004TQP"
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+
 };
 
 const app = initializeApp(firebaseConfig);
